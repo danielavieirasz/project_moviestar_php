785e766f6bc248c2d9f7b816fdeae78d6bd4b07b
Daniela Vieira <danielav.student@hmail.com>
2023-01-12 10:01:45 -0300

    git status

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-    require_once("globals.php")
+    require_once("globals.php");
+    require_once("db.php");
 ?>
 
 <!DOCTYPE html>
@@ -9,17 +10,66 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MovieStar</title>
-    <link rel="short icon" href="img/moviestar.ico"/>
+    <link rel="short icon" href="<?= $BASE_URL ?>img/moviestar.ico"/>
     <!-- Bootstrap  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap.css" integrity="sha512-bR79Bg78Wmn33N5nvkEyg66hNg+xF/Q8NA8YABbj+4sBngYhv9P8eum19hdjYcY7vXk/vRkhM3v/ZndtgEXRWw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- css do projeto -->
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
 </head>
 <body>
-    <H1>Testando MovieStar</H1>
-    <i class="far fa-plus-square"></i>
+ <header>
+    <nav id="main-navbar" class="navbar navbar-expand-lg">
+    <a href="<?= $BASE_URL ?>" class="navbar-brand">
+    <img src="<?= $BASE_URL ?>img/logo.svg" alt="MovieStar" id="logo">
+    <span id="moviestrar-title">MovieStar</span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+    </button>
+    <form action="" id="search-form" class="form-inline my-2 my-lg-0">
+        <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
+        <button class="btn my-2 my-sm-0" type="submit">
+            <i class="fas fa-search"></i>
+        </button>
+    </form>
+    <div class="collapse navbar-collapse" id ="navbar">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entrar / Cadastrar</a>
+
+            </li> 
+        </ul>
+    </div>
+    </nav>
+</header>
+<div id="main-container" class="container-fluid">
+    <h1>Corpo do site</h1>
+</div>
+<footer id="footer">
+    <div class="social-container">
+        <ul>
+            <li>
+                <a href="#"><i class="fab fa-facebook-square"></i></a>
+            </li>
+            <li>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+            </li>
+            <li>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+            </li>
+        </ul>
+    </div>
+    <div id="footer-links-container">
+        <ul>
+            <li><a href="#">Adicionar filme</a></li>
+            <li><a href="#">Adicionar críticia</a></li>
+            <li><a href="#">Entrar / Registrar</a></li>
+        </ul>
+    </div>
+    <p>&copy;2023 Daniela Vieira</p>
+</footer>
     <!-- Bootstrap JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/js/bootstrap.js" integrity="sha512-L6XANV6sOsx9N9c787eDN1pjB2Pzautd3xDgn4cMKuoleHSuCJi5pCDGPCtwE3Bd4A1Olnr0k0aQXbczYzg+wg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
