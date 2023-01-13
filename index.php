e592fb0ea939d8df9f8b341dca20eeb11d2c83ae
Daniela Vieira <danielav.student@hmail.com>
2023-01-13 14:29:44 -0300

    https://github.com/danielavieirasz/project_moviestar_php.git

--- index.php
+++ index.php
@@ -21,14 +21,14 @@
 <body>
  <header>
     <nav id="main-navbar" class="navbar navbar-expand-lg">
-    <a href="<?= $BASE_URL ?>" class="navbar-brand">
-    <img src="<?= $BASE_URL ?>img/logo.svg" alt="MovieStar" id="logo">
-    <span id="moviestrar-title">MovieStar</span>
-    </a>
+        <a href="<?= $BASE_URL ?>" class="navbar-brand">
+        <img src="<?= $BASE_URL ?>img/logo.svg" alt="MovieStar" id="logo">
+        <span id="moviestar-title">MovieStar</span>
+        </a>    
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
         <i class="fas fa-bars"></i>
     </button>
-    <form action="" id="search-form" class="form-inline my-2 my-lg-0">
+    <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
         <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
         <button class="btn my-2 my-sm-0" type="submit">
             <i class="fas fa-search"></i>
@@ -38,7 +38,6 @@
         <ul class="navbar-nav">
             <li class="nav-item">
                 <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entrar / Cadastrar</a>
-
             </li> 
         </ul>
     </div>
@@ -48,7 +47,7 @@
     <h1>Corpo do site</h1>
 </div>
 <footer id="footer">
-    <div class="social-container">
+    <div id="social-container">
         <ul>
             <li>
                 <a href="#"><i class="fab fa-facebook-square"></i></a>
