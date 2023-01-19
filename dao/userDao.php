5014336213f363e035782847381bf0a4e4f8bffe
Daniela Vieira <danielav.student@hmail.com>
2023-01-18 23:10:59 -0300

    main

---
+++ dao/userDao.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once("models/User.php");
+
+ class UserDAO implements UserDAOInterface{
+
+    $private $conn;
+    $private $url;
+
+    public function __construct(PDO $conn, $url) {
+        $this->conn = $conn;
+        $this->url = $url;
+    }
+
+    public function buildUser($data) {
+        /* CONTINUAÇÃO AQUI*/
+    }
+
+    public function create(User $user, $authUser = false) {
+
+    }
+
+    public function update(User $user) {
+
+    }
+
+    public function verifyToken($protected =false) {
+
+    }
+    public function setTokenToSession($token, $redirect= true) {
+
+    }
+
+    public function authenticateUser($email, $password) {
+
+    }
+
+    public function findByEmail ($email) {
+
+    }
+
+    public function findById($id) {
+
+    }
+
+    public function findByToken($token) {
+
+    }
+
+    public function changePassword(User $user) {
+
+    }
+
+ }
